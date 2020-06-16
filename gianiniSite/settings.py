3f49de63e6ad27cf4873126f9f504575b2e7e9bd
Tracun <lucas060994@gmail.com>
2020-06-16 11:40:29 -0300

    Remove heroku settings

--- gianiniSite/settings.py
+++ gianiniSite/settings.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 # Configure Django App for Heroku.
-import django_heroku
+#import django_heroku
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
